88aa7db362abd62670358e295c07ff76cb2ca07d
XiongYouliang <xiongayou@gmail.com>
2021-07-30 21:12:03 +0800

    code review： Use tabs for intend; header file include review

--- Source/FairyGUI/Private/Widgets/SDisplayObject.cpp
+++ Source/FairyGUI/Private/Widgets/SDisplayObject.cpp
@@ -1,45 +1,46 @@
 #include "Widgets/SDisplayObject.h"
 #include "Framework/Application/SlateApplication.h"
 #include "FairyApplication.h"
+#include "UI/FairyObject.h"
 //#include "Engine/GameViewportClient.h" // Slate Widget shouldn't reference Engine module class
 
 FNoChildren SDisplayObject::NoChildrenInstance;
 FName SDisplayObject::SDisplayObjectTag("SDisplayObjectTag");
 
 SDisplayObject::SDisplayObject() :
-    bVisible(true),
-    bInteractable(true),
-    bTouchable(true),
-    bOpaque(true)
+	bVisible(true),
+	bInteractable(true),
+	bTouchable(true),
+	bOpaque(true)
 {
-    SetCanTick(false);
-    bCanSupportFocus = false;
+	SetCanTick(false);
+	bCanSupportFocus = false;
 }
 
 void SDisplayObject::Construct(const SDisplayObject::FArguments& InArgs)
 {
-    FairyObject = InArgs._GObject;
-    SetTag(InArgs._Tag);
+	FairyObject = InArgs._GObject;
+	SetTag(InArgs._Tag);
 }
 
 FVector2D SDisplayObject::GetInViewSize()
 {
-    FVector2D InViewSize(ForceInit);
-    if (FairyObject.IsValid())
-    {
-        InViewSize = FairyObject->GetRelationSize();
-    }
-    return InViewSize;
+	FVector2D InViewSize(ForceInit);
+	if (FairyObject.IsValid())
+	{
+		InViewSize = FairyObject->GetRelationSize();
+	}
+	return InViewSize;
 }
 
 FVector2D SDisplayObject::GetInViewSize() const
 {
-    FVector2D InViewSize(ForceInit);
-    if (FairyObject.IsValid())
-    {
-        InViewSize = FairyObject->GetRelationSize();
-    }
-    return InViewSize;
+	FVector2D InViewSize(ForceInit);
+	if (FairyObject.IsValid())
+	{
+		InViewSize = FairyObject->GetRelationSize();
+	}
+	return InViewSize;
 }
 
 void SDisplayObject::EnsureSizeCorrect()
@@ -49,74 +50,74 @@ void SDisplayObject::EnsureSizeCorrect()
 
 void SDisplayObject::SetVisible(bool bInVisible)
 {
-    if (bVisible != bInVisible)
-    {
-        bVisible = bInVisible;
-        UpdateVisibilityFlags();
-    }
+	if (bVisible != bInVisible)
+	{
+		bVisible = bInVisible;
+		UpdateVisibilityFlags();
+	}
 }
 
 void SDisplayObject::SetTouchable(bool bInTouchable)
 {
-    if (bTouchable != bInTouchable)
-    {
-        bTouchable = bInTouchable;
-        UpdateVisibilityFlags();
-    }
+	if (bTouchable != bInTouchable)
+	{
+		bTouchable = bInTouchable;
+		UpdateVisibilityFlags();
+	}
 }
 
 void SDisplayObject::SetOpaque(bool bInOpaque)
 {
-    if (bOpaque != bInOpaque)
-    {
-        bOpaque = bInOpaque;
-        UpdateVisibilityFlags();
-    }
+	if (bOpaque != bInOpaque)
+	{
+		bOpaque = bInOpaque;
+		UpdateVisibilityFlags();
+	}
 }
 
 void SDisplayObject::SetInteractable(bool bInInteractable)
 {
-    if (bInteractable != bInInteractable)
-    {
-        bInteractable = bInInteractable;
-        UpdateVisibilityFlags();
-    }
+	if (bInteractable != bInInteractable)
+	{
+		bInteractable = bInInteractable;
+		UpdateVisibilityFlags();
+	}
 }
 
 void SDisplayObject::UpdateVisibilityFlags()
 {
-    bool HitTestFlag = bInteractable && bTouchable;
-    if (!bVisible)
-    {
-        SetVisibility(EVisibility::Collapsed);
-    }
-    else if (!HitTestFlag)
-    {
-        SetVisibility(EVisibility::HitTestInvisible);
-    }
-    else if (!bOpaque)
-    {
-        SetVisibility(EVisibility::SelfHitTestInvisible);
-    }
-    else
-    {
-        SetVisibility(EVisibility::All);
-    }
+	bool HitTestFlag = bInteractable && bTouchable;
+	if (!bVisible)
+	{
+		SetVisibility(EVisibility::Collapsed);
+	}
+	else if (!HitTestFlag)
+	{
+		SetVisibility(EVisibility::HitTestInvisible);
+	}
+	else if (!bOpaque)
+	{
+		SetVisibility(EVisibility::SelfHitTestInvisible);
+	}
+	else
+	{
+		SetVisibility(EVisibility::All);
+	}
 }
 
 FVector2D SDisplayObject::ComputeDesiredSize(float) const
 {
-    FVector2D InViewSize = FVector2D::ZeroVector;
-    if (FairyObject.IsValid())
-    {
-        InViewSize = FairyObject->GetRelationSize();
-    }
-    return InViewSize;
+	FVector2D InViewSize = FVector2D::ZeroVector;
+	if (FairyObject.IsValid())
+	{
+		InViewSize = FairyObject->GetRelationSize();
+	}
+	return InViewSize;
 }
 
 FChildren* SDisplayObject::GetChildren()
 {
-    return &NoChildrenInstance;
+	return &NoChildrenInstance;
 }
 
 void SDisplayObject::OnArrangeChildren(const FGeometry& AllottedGeometry, FArrangedChildren& ArrangedChildren) const
@@ -125,58 +126,58 @@ void SDisplayObject::OnArrangeChildren(const FGeometry& AllottedGeometry, FArran
 
 int32 SDisplayObject::OnPaint(const FPaintArgs& Args, const FGeometry& AllottedGeometry, const FSlateRect& MyCullingRect, FSlateWindowElementList& OutDrawElements, int32 LayerId, const FWidgetStyle& InWidgetStyle, bool bParentEnabled) const
 {
-    return LayerId;
+	return LayerId;
 }
 
 bool SDisplayObject::OnStage() const
 {
-    if (!IsParentValid())
-    {
-        return false;
-    }
+	if (!IsParentValid())
+	{
+		return false;
+	}
 
-    TSharedPtr<SWindow> Window = FSlateApplication::Get().FindWidgetWindow(AsShared());
-    if (Window.IsValid())
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+	TSharedPtr<SWindow> Window = FSlateApplication::Get().FindWidgetWindow(AsShared());
+	if (Window.IsValid())
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 FReply SDisplayObject::OnMouseButtonDown(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent)
 {
-    return UFairyApplication::Get()->OnWidgetMouseButtonDown(AsShared(), MyGeometry, MouseEvent);
+	return UFairyApplication::Get()->OnWidgetMouseButtonDown(AsShared(), MyGeometry, MouseEvent);
 }
 
 FReply SDisplayObject::OnMouseButtonUp(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent)
 {
-    return UFairyApplication::Get()->OnWidgetMouseButtonUp(AsShared(), MyGeometry, MouseEvent);
+	return UFairyApplication::Get()->OnWidgetMouseButtonUp(AsShared(), MyGeometry, MouseEvent);
 }
 
 FReply SDisplayObject::OnMouseMove(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent)
 {
-    return UFairyApplication::Get()->OnWidgetMouseMove(AsShared(), MyGeometry, MouseEvent);
+	return UFairyApplication::Get()->OnWidgetMouseMove(AsShared(), MyGeometry, MouseEvent);
 }
 
 FReply SDisplayObject::OnMouseButtonDoubleClick(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent)
 {
-    return UFairyApplication::Get()->OnWidgetMouseButtonDoubleClick(AsShared(), MyGeometry, MouseEvent);
+	return UFairyApplication::Get()->OnWidgetMouseButtonDoubleClick(AsShared(), MyGeometry, MouseEvent);
 }
 
 void SDisplayObject::OnMouseEnter(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent)
 {
-    return UFairyApplication::Get()->OnWidgetMouseEnter(AsShared(), MyGeometry, MouseEvent);
+	return UFairyApplication::Get()->OnWidgetMouseEnter(AsShared(), MyGeometry, MouseEvent);
 }
 
 void SDisplayObject::OnMouseLeave(const FPointerEvent& MouseEvent)
 {
-    return UFairyApplication::Get()->OnWidgetMouseLeave(AsShared(), MouseEvent);
+	return UFairyApplication::Get()->OnWidgetMouseLeave(AsShared(), MouseEvent);
 }
 
 FReply SDisplayObject::OnMouseWheel(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent)
 {
-    return UFairyApplication::Get()->OnWidgetMouseWheel(AsShared(), MyGeometry, MouseEvent);
+	return UFairyApplication::Get()->OnWidgetMouseWheel(AsShared(), MyGeometry, MouseEvent);
 }
--- Source/FairyGUI/Public/Widgets/SDisplayObject.h
+++ Source/FairyGUI/Public/Widgets/SDisplayObject.h
@@ -3,7 +3,6 @@
 #include "CoreMinimal.h"
 #include "UObject/NoExportTypes.h"
 #include "Slate.h"
-#include "UI/FairyObject.h"
 
 class UFairyObject;
 
@@ -14,66 +13,65 @@ class UFairyObject;
 class FAIRYGUI_API SDisplayObject : public SWidget
 {
 public:
-    SLATE_BEGIN_ARGS(SDisplayObject) :
-        _GObject(nullptr),
-        _Tag(SDisplayObject::SDisplayObjectTag)
-    {
-    }
-        SLATE_ARGUMENT(UFairyObject*, GObject)
-        SLATE_ARGUMENT(FName, Tag)
-    SLATE_END_ARGS()
+	SLATE_BEGIN_ARGS(SDisplayObject) :
+		_GObject(nullptr),
+		_Tag(SDisplayObject::SDisplayObjectTag)
+	{}
+		SLATE_ARGUMENT(class UFairyObject*, GObject)
+		SLATE_ARGUMENT(FName, Tag)
+	SLATE_END_ARGS()
 
 public:
-    static FName SDisplayObjectTag;
+	static FName SDisplayObjectTag;
 
-    SDisplayObject();
-    void Construct(const FArguments& InArgs);
+	SDisplayObject();
+	void Construct(const FArguments& InArgs);
 
-    void SetVisible(bool bInVisible);
-    bool IsVisible() const { return bVisible; }
+	void SetVisible(bool bInVisible);
+	bool IsVisible() const { return bVisible; }
 
-    void SetTouchable(bool bInTouchable);
-    bool IsTouchable() const { return bTouchable; }
+	void SetTouchable(bool bInTouchable);
+	bool IsTouchable() const { return bTouchable; }
 
-    void SetOpaque(bool bInOpaque);
-    bool IsOpaque() const { return bOpaque; }
+	void SetOpaque(bool bInOpaque);
+	bool IsOpaque() const { return bOpaque; }
 
-    void SetInteractable(bool bInInteractable);
-    virtual bool IsInteractable() const override { return bInteractable; }
+	void SetInteractable(bool bInInteractable);
+	virtual bool IsInteractable() const override { return bInteractable; }
 
-    virtual FReply OnMouseButtonDown(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) override;
-    virtual FReply OnMouseButtonUp(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) override;
-    virtual FReply OnMouseMove(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) override;
-    virtual FReply OnMouseButtonDoubleClick(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) override;
-    virtual void OnMouseEnter(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) override;
-    virtual void OnMouseLeave(const FPointerEvent& MouseEvent) override;
-    virtual FReply OnMouseWheel(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) override;
+	virtual FReply OnMouseButtonDown(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) override;
+	virtual FReply OnMouseButtonUp(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) override;
+	virtual FReply OnMouseMove(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) override;
+	virtual FReply OnMouseButtonDoubleClick(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) override;
+	virtual void OnMouseEnter(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) override;
+	virtual void OnMouseLeave(const FPointerEvent& MouseEvent) override;
+	virtual FReply OnMouseWheel(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) override;
 
-    bool OnStage() const;
+	bool OnStage() const;
 
-    FVector2D GetInViewSize();
-    FVector2D GetInViewSize() const;
-    TWeakObjectPtr<UFairyObject> GetFairyObject() const { return FairyObject; };
-    TWeakObjectPtr<UFairyObject> GetFairyObject() { return FairyObject; };
-
-private:
-    TWeakObjectPtr<class UFairyObject> FairyObject;
-
-protected:
-    virtual FVector2D ComputeDesiredSize(float LayoutScaleMultiplier) const override;
-    virtual FChildren* GetChildren() override;
-    virtual void OnArrangeChildren(const FGeometry& AllottedGeometry, FArrangedChildren& ArrangedChildren) const override;
-    virtual int32 OnPaint(const FPaintArgs& Args, const FGeometry& AllottedGeometry, const FSlateRect& MyCullingRect, FSlateWindowElementList& OutDrawElements, int32 LayerId, const FWidgetStyle& InWidgetStyle, bool bParentEnabled) const override;
-    virtual void EnsureSizeCorrect();
-
-    void UpdateVisibilityFlags();
-
-protected:
-    bool bVisible : 1;
-    bool bInteractable : 1;
-    bool bTouchable : 1;
-    bool bOpaque : 1;
+	FVector2D GetInViewSize();
+	FVector2D GetInViewSize() const;
+	TWeakObjectPtr<class UFairyObject> GetFairyObject() const { return FairyObject; };
+	TWeakObjectPtr<class UFairyObject> GetFairyObject() { return FairyObject; };
 
 private:
-    static FNoChildren NoChildrenInstance;
+	TWeakObjectPtr<class UFairyObject> FairyObject;
+
+protected:
+	virtual FVector2D ComputeDesiredSize(float LayoutScaleMultiplier) const override;
+	virtual FChildren* GetChildren() override;
+	virtual void OnArrangeChildren(const FGeometry& AllottedGeometry, FArrangedChildren& ArrangedChildren) const override;
+	virtual int32 OnPaint(const FPaintArgs& Args, const FGeometry& AllottedGeometry, const FSlateRect& MyCullingRect, FSlateWindowElementList& OutDrawElements, int32 LayerId, const FWidgetStyle& InWidgetStyle, bool bParentEnabled) const override;
+	virtual void EnsureSizeCorrect();
+
+	void UpdateVisibilityFlags();
+
+protected:
+	bool bVisible : 1;
+	bool bInteractable : 1;
+	bool bTouchable : 1;
+	bool bOpaque : 1;
+
+private:
+	static FNoChildren NoChildrenInstance;
 };

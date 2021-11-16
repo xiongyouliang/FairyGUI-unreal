db673566ff6f4e851230931ecc1d84969110f172
XiongYouliang <xiongayou@gmail.com>
2021-11-16 22:01:27 +0800

    Add Tweener object Container and add mamager method.

--- Source/FairyGUI/Private/Tween/TweenManager.cpp
+++ Source/FairyGUI/Private/Tween/TweenManager.cpp
@@ -3,201 +3,225 @@
 
 FTweenManager::FTweenManager()
 {
-    TotalActiveTweenerNum = 0;
-    ActiveTweenerPointerCapcity = 30;
+	TotalActiveTweenerNum = 0;
+	ActiveTweenerPointerCapcity = 30;
 	ActiveTweenerPointerArray = new FairyTweenerPointer[ActiveTweenerPointerCapcity]();
 }
 
 FTweenManager::~FTweenManager()
 {
-    for (auto it : TweenerPool)
-    {
+	for (auto it : TweenerPool)
+	{
 		delete it;
-    }
+	}
 
-    int32 cnt = TotalActiveTweenerNum;
-    for (int32 i = 0; i < cnt; i++)
-    {
-        FFairyTweener* tweener = ActiveTweenerPointerArray[i];
-        if (tweener != nullptr)
-        {
+	int32 cnt = TotalActiveTweenerNum;
+	for (int32 i = 0; i < cnt; i++)
+	{
+		FFairyTweener* tweener = ActiveTweenerPointerArray[i];
+		if (tweener != nullptr)
+		{
 			delete tweener;
-        }
-    }
-    delete []ActiveTweenerPointerArray;
+		}
+	}
+	delete []ActiveTweenerPointerArray;
 }
 
 FFairyTweener* FTweenManager::CreateTweener()
 {
-    FFairyTweener* tweener = nullptr;
-    int32 cnt = TweenerPool.Num();
-    if (cnt > 0)
-    {
-        tweener = TweenerPool.Pop();
-        tweener->Handle.IncreaseSerialNumber();
-    }
-    else
-    {
-        TweenerInstanceCount++;
-        if (!ensureMsgf(TweenerInstanceCount != FTweenerHandle::MaxIndex, TEXT("Tweener index number has wrapped around!")))
-        {
-            TweenerInstanceCount = 0;
-        }
-        tweener = new FFairyTweener();
-        tweener->Handle.SetIndex(TweenerInstanceCount);
-    }
-    tweener->Init();
-    ActiveTweenerPointerArray[TotalActiveTweenerNum++] = tweener;
+	FFairyTweener* tweener = nullptr;
+	int32 cnt = TweenerPool.Num();
+	if (cnt > 0)
+	{
+		tweener = TweenerPool.Pop();
+		tweener->Handle.IncreaseSerialNumber();
+	}
+	else
+	{
+		TweenerInstanceCount++;
+		if (!ensureMsgf(TweenerInstanceCount != FTweenerHandle::MaxIndex, TEXT("Tweener index number has wrapped around!")))
+		{
+			TweenerInstanceCount = 0;
+		}
+		tweener = new FFairyTweener();
+		tweener->Handle.SetIndex(TweenerInstanceCount);
+	}
+	tweener->Init();
+	ActiveTweenerPointerArray[TotalActiveTweenerNum++] = tweener;
 
-    if (TotalActiveTweenerNum == ActiveTweenerPointerCapcity)
-    {
-        int32 newCapcity = ActiveTweenerPointerCapcity + FMath::CeilToInt(ActiveTweenerPointerCapcity * 0.5f);
+	if (TotalActiveTweenerNum == ActiveTweenerPointerCapcity)
+	{
+		int32 newCapcity = ActiveTweenerPointerCapcity + FMath::CeilToInt(ActiveTweenerPointerCapcity * 0.5f);
 		FFairyTweener** newArray = new FairyTweenerPointer[newCapcity];
-        FMemory::Memcpy(newArray, ActiveTweenerPointerArray, ActiveTweenerPointerCapcity * sizeof(FFairyTweener*));
-        delete []ActiveTweenerPointerArray;
-        ActiveTweenerPointerArray = newArray;
-        ActiveTweenerPointerCapcity = newCapcity;
-    }
+		FMemory::Memcpy(newArray, ActiveTweenerPointerArray, ActiveTweenerPointerCapcity * sizeof(FFairyTweener*));
+		delete []ActiveTweenerPointerArray;
+		ActiveTweenerPointerArray = newArray;
+		ActiveTweenerPointerCapcity = newCapcity;
+	}
 
-    return tweener;
+	return tweener;
+}
+
+void FTweenManager::AddTweener(TSharedPtr<FFairyTweener> &InTweener, UFairyObject* InTarget, bool InPaused)
+{
+	if (InTweener.IsValid())
+	{
+		TweenerArray& Array = TweenerTable.FindOrAdd(TweenerTableKey(InTarget));
+		InTweener->bPaused = InPaused;
+		Array.Add(InTweener);
+	}
+}
+
+void FTweenManager::RemoveTweener(TSharedPtr<FFairyTweener> &InTweener)
+{
+	for (auto& Pair : TweenerTable)
+	{
+		TweenerArray& Array = Pair.Value;
+		for (int i = 0; i < Array.Num(); i++)
+		{
+			TSharedPtr<FFairyTweener>& Element = Array[i];
+			if (Element.Get() == InTweener.Get())
+			{
+				Array.RemoveAt(i);
+				break;
+			}
+		}
+	}
+}
+
+void FTweenManager::RemoveTweenerWithTarget(TSharedPtr<FFairyTweener> &InTweener, UFairyObject* InTarget)
+{
+	TweenerTableKey TargetKey = TweenerTableKey(InTarget);
+	if ( TweenerTable.Contains(TargetKey) )
+	{
+		TweenerArray* Array = TweenerTable.Find(TargetKey);
+		for (int i = 0; i < Array->Num(); i++)
+		{
+			TweenerArrayElement element = (*Array)[i];
+			if (element.Get() == InTweener.Get())
+			{
+				Array->RemoveAt(i);
+				break;
+			}
+		}
+	}
+}
+
+void FTweenManager::RemoveAllTweenerWithTarget(UFairyObject* InTarget)
+{
+	TweenerTableKey TargetKey = TweenerTableKey(InTarget);
+	if ( TweenerTable.Contains(TargetKey) )
+	{
+		TweenerTable.Remove(TargetKey);
+	}
 }
 
 bool FTweenManager::KillTween(FTweenerHandle & Handle, bool bCompleted)
 {
-    int32 cnt = TotalActiveTweenerNum;
-    for (int32 i = 0; i < cnt; i++)
-    {
-        FFairyTweener* tweener = ActiveTweenerPointerArray[i];
-        if (tweener != nullptr && tweener->Handle == Handle && !tweener->bKilled)
-        {
-            Handle.Invalidate();
-            tweener->Kill(bCompleted);
-            return true;
-        }
-    }
+	int32 cnt = TotalActiveTweenerNum;
+	for (int32 i = 0; i < cnt; i++)
+	{
+		FFairyTweener* tweener = ActiveTweenerPointerArray[i];
+		if (tweener != nullptr && tweener->Handle == Handle && !tweener->bKilled)
+		{
+			Handle.Invalidate();
+			tweener->Kill(bCompleted);
+			return true;
+		}
+	}
 
-    Handle.Invalidate();
-    return false;
+	Handle.Invalidate();
+	return false;
 }
 
 bool FTweenManager::KillTweens(UObject* Target, bool bCompleted)
 {
-    if (Target == nullptr)
-    {
-        return false;
-    }
+	if (Target == nullptr)
+	{
+		return false;
+	}
 
-    bool result = false;
-    int32 cnt = TotalActiveTweenerNum;
-    for (int32 i = 0; i < cnt; i++)
-    {
-        FFairyTweener* tweener = ActiveTweenerPointerArray[i];
-        if (tweener != nullptr && tweener->Target.Get() == Target && !tweener->bKilled)
-        {
-            tweener->Kill(bCompleted);
+	bool result = false;
+	int32 cnt = TotalActiveTweenerNum;
+	for (int32 i = 0; i < cnt; i++)
+	{
+		FFairyTweener* tweener = ActiveTweenerPointerArray[i];
+		if (tweener != nullptr && tweener->Target.Get() == Target && !tweener->bKilled)
+		{
+			tweener->Kill(bCompleted);
 			result = true;
-        }
-    }
+		}
+	}
 
 	return result;
 }
 
 FFairyTweener* FTweenManager::GetTween(FTweenerHandle const& Handle)
 {
-    if (!Handle.IsValid())
-    {
+	if (!Handle.IsValid())
+	{
 		return nullptr;
-    }
+	}
 
-    int32 cnt = TotalActiveTweenerNum;
-    for (int32 i = 0; i < cnt; i++)
-    {
-        FFairyTweener* tweener = ActiveTweenerPointerArray[i];
-        if (tweener != nullptr && tweener->Handle == Handle && !tweener->bKilled)
-        {
-            return tweener;
-        }
-    }
+	int32 cnt = TotalActiveTweenerNum;
+	for (int32 i = 0; i < cnt; i++)
+	{
+		FFairyTweener* tweener = ActiveTweenerPointerArray[i];
+		if (tweener != nullptr && tweener->Handle == Handle && !tweener->bKilled)
+		{
+			return tweener;
+		}
+	}
 
-    return nullptr;
+	return nullptr;
 }
 
 FFairyTweener* FTweenManager::GetTween(UObject* Target)
 {
-    if (Target == nullptr)
-    {
+	if (Target == nullptr)
+	{
 		return nullptr;
-    }
+	}
 
-    int32 cnt = TotalActiveTweenerNum;
-    for (int32 i = 0; i < cnt; i++)
-    {
-        FFairyTweener* tweener = ActiveTweenerPointerArray[i];
-        if (tweener != nullptr && tweener->Target.Get() == Target && !tweener->bKilled)
-        {
-            return tweener;
-        }
-    }
+	int32 cnt = TotalActiveTweenerNum;
+	for (int32 i = 0; i < cnt; i++)
+	{
+		FFairyTweener* tweener = ActiveTweenerPointerArray[i];
+		if (tweener != nullptr && tweener->Target.Get() == Target && !tweener->bKilled)
+		{
+			return tweener;
+		}
+	}
 
-    return nullptr;
+	return nullptr;
 }
 
 void FTweenManager::Tick(float DeltaTime)
 {
-    int32 cnt = TotalActiveTweenerNum;
-    int32 freePosStart = -1;
-    for (int32 i = 0; i < cnt; i++)
-    {
-        FFairyTweener* tweener = ActiveTweenerPointerArray[i];
-        if (tweener == nullptr)
-        {
-            if (freePosStart == -1)
-            {
-				freePosStart = i;
-            }
-        }
-        else if (tweener->bKilled)
-        {
-            tweener->Reset();
-            TweenerPool.Add(tweener);
-            ActiveTweenerPointerArray[i] = nullptr;
+	for (auto& Pair : TweenerTable)
+	{
+		TweenerArray& Array = Pair.Value;
+		for (size_t i = 0; i < Array.Num(); i++)
+		{
+			TweenerArrayElement element = Array[i];
+			if (!element->IsPaused())
+			{
+				if (element->IsCompleted() || !element->IsTargetValid())
+				{
+					CompletedArray.Add(element);
+					continue;
+				}
+				else
+				{
+					element->Update(DeltaTime);
+				}
+			}
+		}
+	}
 
-            if (freePosStart == -1)
-            {
-				freePosStart = i;
-            }
-        }
-        else
-        {
-            if (tweener->Target.IsStale())
-            {
-				tweener->bKilled = true;
-            }
-            else if (!tweener->bPaused)
-            {
-				tweener->Update(DeltaTime);
-            }
-
-            if (freePosStart != -1)
-            {
-                ActiveTweenerPointerArray[freePosStart] = tweener;
-                ActiveTweenerPointerArray[i] = nullptr;
-                freePosStart++;
-            }
-        }
-    }
-
-    if (freePosStart >= 0)
-    {
-        if (TotalActiveTweenerNum != cnt) //new tweens added
-        {
-            int32 j = cnt;
-            cnt = TotalActiveTweenerNum - cnt;
-            for (int32 i = 0; i < cnt; i++)
-            {
-				ActiveTweenerPointerArray[freePosStart++] = ActiveTweenerPointerArray[j++];
-            }
-        }
-        TotalActiveTweenerNum = freePosStart;
-    }
+	for (size_t i = 0; i < CompletedArray.Num(); i++)
+	{
+		TweenerArrayElement element = CompletedArray[i];
+		RemoveTweener(element);
+	}
 }
--- Source/FairyGUI/Public/Tween/TweenManager.h
+++ Source/FairyGUI/Public/Tween/TweenManager.h
@@ -8,36 +8,46 @@
 class UFairyObject;
 
 typedef FFairyTweener* FairyTweenerPointer;
+typedef TWeakObjectPtr<UFairyObject> TweenerTableKey;
+typedef TSharedPtr<FFairyTweener> TweenerArrayElement;
+typedef TArray<TweenerArrayElement> TweenerArray;
 
 class FAIRYGUI_API FTweenManager : public FTickableGameObject
 {
 public:
-    FTweenManager();
-    ~FTweenManager();
+	FTweenManager();
+	~FTweenManager();
 
-    FFairyTweener* CreateTweener();
+	FFairyTweener* CreateTweener();
 
-    bool IsTweening(FTweenerHandle const& Handle) { return GetTween(Handle) != nullptr; }
-    bool IsTweening(UObject* Target) { return GetTween(Target) != nullptr; }
+	void AddTweener(TSharedPtr<FFairyTweener> &InTweener, UFairyObject* InTarget, bool InPaused);
+	void RemoveTweener(TSharedPtr<FFairyTweener> &InTweener);
+	void RemoveTweenerWithTarget(TSharedPtr<FFairyTweener> &InTweener, UFairyObject* InTarget);
+	void RemoveAllTweenerWithTarget(UFairyObject* InTarget);
 
-    bool KillTween(FTweenerHandle & Handle, bool bCompleted);
-    bool KillTweens(UObject* Target, bool bCompleted);
+	bool IsTweening(FTweenerHandle const& Handle) { return GetTween(Handle) != nullptr; }
+	bool IsTweening(UObject* Target) { return GetTween(Target) != nullptr; }
 
-    FFairyTweener* GetTween(FTweenerHandle const& Handle);
-    FFairyTweener* GetTween(UObject* Target);
+	bool KillTween(FTweenerHandle & Handle, bool bCompleted);
+	bool KillTweens(UObject* Target, bool bCompleted);
 
-    void Tick(float DeltaTime);
-    TStatId GetStatId() const 
-    {
-        return TStatId();
-    }
+	FFairyTweener* GetTween(FTweenerHandle const& Handle);
+	FFairyTweener* GetTween(UObject* Target);
+
+	void Tick(float DeltaTime);
+	TStatId GetStatId() const 
+	{
+		return TStatId();
+	}
 
 private:
-    FairyTweenerPointer* ActiveTweenerPointerArray;
-    int32 ActiveTweenerPointerCapcity;
-    int32 TotalActiveTweenerNum;
-    uint32 TweenerInstanceCount;
+	FairyTweenerPointer* ActiveTweenerPointerArray;
+	int32 ActiveTweenerPointerCapcity;
+	int32 TotalActiveTweenerNum;
+	uint32 TweenerInstanceCount;
 
-    TArray<FairyTweenerPointer> TweenerPool;
+	TArray<FFairyTweener*> TweenerPool;
+	TMap<TweenerTableKey, TweenerArray> TweenerTable;
+	TweenerArray CompletedArray;
 };
 

7e2796e60554a6eb03a71f7bba9c06bdee02c8b1
XiongYouliang <xiongayou@gmail.com>
2021-11-23 15:02:54 +0800

    Add more tweener create function in UTweenManager class.

--- Source/FairyGUI/Private/Tween/TweenManager.cpp
+++ Source/FairyGUI/Private/Tween/TweenManager.cpp
@@ -1,7 +1,4 @@
 #include "Tween/TweenManager.h"
-#include "Tween/FairyTweener.h"
-#include "Tween/FairyTweenerInterval.h"
-#include "Tween/FairyTweenerInstant.h"
 
 #include "FairyApplication.h"
 
@@ -32,6 +29,48 @@ UFairyTweenerPos* UTweenManager::CreateTweenerPos(float InDuration, FVector2D In
 	return Tweener;
 }
 
+UFairyTweenerSize* UTweenManager::CreateTweenerSize(float InDuration, FVector2D InStartSize, FVector2D InDstSize)
+{
+	UFairyTweenerSize* Tweener = NewObject<UFairyTweenerSize>(this);
+	Tweener->Init(InDuration, InStartSize, InDstSize);
+	return Tweener;
+}
+
+UFairyTweenerOpacity* UTweenManager::CreateTweenerOpacity(float InDuration, float InStartOpacity, float InDstOpacity)
+{
+	UFairyTweenerOpacity* Tweener = NewObject<UFairyTweenerOpacity>(this);
+	Tweener->Init(InDuration, InStartOpacity, InDstOpacity);
+	return Tweener;
+}
+
+UFairyTweenerRotate* UTweenManager::CreateTweenerRotate(float InDuration, float InStartRotate, float InDstRotate)
+{
+	UFairyTweenerRotate* Tweener = NewObject<UFairyTweenerRotate>(this);
+	Tweener->Init(InDuration, InStartRotate, InDstRotate);
+	return Tweener;
+}
+
+UFairyTweenerScale* UTweenManager::CreateTweenerScale(float InDuration, FVector2D InStartScale, FVector2D InDstScale)
+{
+	UFairyTweenerScale* Tweener = NewObject<UFairyTweenerScale>(this);
+	Tweener->Init(InDuration, InStartScale, InDstScale);
+	return Tweener;
+}
+
+UFairyTweenerSkew* UTweenManager::CreateTweenerSkew(float InDuration, FVector2D InStartSkew, FVector2D InDstSkew)
+{
+	UFairyTweenerSkew* Tweener = NewObject<UFairyTweenerSkew>(this);
+	Tweener->Init(InDuration, InStartSkew, InDstSkew);
+	return Tweener;
+}
+
+UFairyTweenerColor* UTweenManager::CreateTweenerColor(float InDuration, FColor InStartColor, FColor InDstColor)
+{
+	UFairyTweenerColor* Tweener = NewObject<UFairyTweenerColor>(this);
+	Tweener->Init(InDuration, InStartColor, InDstColor);
+	return Tweener;
+}
+
 UFairyTweenerSequence* UTweenManager::CreateTweenerSequence(const TArray<UFairyTweenerFiniteTime*>& InTweenerList)
 {
 	UFairyTweenerSequence* Tweener = NewObject<UFairyTweenerSequence>(this);
@@ -44,6 +83,23 @@ UFairyTweenerDelay* UTweenManager::CreateTweenerDelay(float InDelayTime)
 {
 	UFairyTweenerDelay* Tweener = NewObject<UFairyTweenerDelay>(this);
 	Tweener->Init(InDelayTime);
+	return Tweener;
+}
+
+UFairyTweenerShow* UTweenManager::CreateTweenerShow()
+{
+	return NewObject<UFairyTweenerShow>(this);
+}
+
+UFairyTweenerHide* UTweenManager::CreateTweenerHide()
+{
+	return NewObject<UFairyTweenerHide>(this);
+}
+
+UFairyTweenerPivot* UTweenManager::CreateTweenerPivot(FVector2D InDstPivot)
+{
+	UFairyTweenerPivot* Tweener = NewObject<UFairyTweenerPivot>(this);
+	Tweener->Init(InDstPivot);
 	return Tweener;
 }
 
--- Source/FairyGUI/Public/Tween/TweenManager.h
+++ Source/FairyGUI/Public/Tween/TweenManager.h
@@ -3,17 +3,12 @@
 #include "CoreMinimal.h"
 #include "Engine/Engine.h"
 #include "Tickable.h"
+#include "UI/FairyObject.h"
 #include "Tween/FairyTweener.h"
+#include "Tween/FairyTweenerInterval.h"
+#include "Tween/FairyTweenerInstant.h"
 
 #include "TweenManager.generated.h"
-
-class UFairyObject;
-class UFairyTweener;
-class UFairyTweenerFiniteTime;
-class UFairyTweenerPos;
-class UFairyTweenerSequence;
-class UFairyTweenerCallFunc;
-class UFairyTweenerDynamicCallFunc;
 
 typedef UFairyTweener* FairyTweenerPointer;
 
@@ -30,6 +25,24 @@ public:
 
 	UFUNCTION(BlueprintCallable, Category = "FairyGUI")
 	UFairyTweenerPos* CreateTweenerPos(float InDuration, FVector2D InStartPos, FVector2D InDstPos);
+	
+	UFUNCTION(BlueprintCallable, Category = "FairyGUI")
+	UFairyTweenerSize* CreateTweenerSize(float InDuration, FVector2D InStartSize, FVector2D InDstSize);
+
+	UFUNCTION(BlueprintCallable, Category = "FairyGUI")
+	UFairyTweenerOpacity* CreateTweenerOpacity(float InDuration, float InStartOpacity, float InDstOpacity);
+
+	UFUNCTION(BlueprintCallable, Category = "FairyGUI")
+	UFairyTweenerRotate* CreateTweenerRotate(float InDuration, float InStartroRotate, float InDstRotate);
+	
+	UFUNCTION(BlueprintCallable, Category = "FairyGUI")
+	UFairyTweenerScale* CreateTweenerScale(float InDuration, FVector2D InStartScale, FVector2D InDstScale);
+	
+	UFUNCTION(BlueprintCallable, Category = "FairyGUI")
+	UFairyTweenerSkew* CreateTweenerSkew(float InDuration, FVector2D InStartSkew, FVector2D InDstSkew);
+	
+	UFUNCTION(BlueprintCallable, Category = "FairyGUI")
+	UFairyTweenerColor* CreateTweenerColor(float InDuration, FColor InStartColor, FColor InDstColor);
 
 	UFUNCTION(BlueprintCallable, Category = "FairyGUI")
 	UFairyTweenerSequence* CreateTweenerSequence(const TArray<UFairyTweenerFiniteTime*>& InTweenerList);
@@ -37,7 +50,18 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "FairyGUI")
 	UFairyTweenerDelay* CreateTweenerDelay(float InDelayTime);
 
+	UFUNCTION(BlueprintCallable, Category = "FairyGUI")
+	UFairyTweenerShow* CreateTweenerShow();
+
+	UFUNCTION(BlueprintCallable, Category = "FairyGUI")
+	UFairyTweenerHide* CreateTweenerHide();
+	
+	UFUNCTION(BlueprintCallable, Category = "FairyGUI")
+	UFairyTweenerPivot* CreateTweenerPivot(FVector2D InDstPivot);
+
+	// non-dynamic delegate can't pass in blueprint, so this is not a UFUNCTION
 	UFairyTweenerCallFunc* CreateTweenerCallFunc(const FTweenDelegate& InDelegate);	
+	
 	UFUNCTION(BlueprintCallable, Category = "FairyGUI")
 	UFairyTweenerDynamicCallFunc* CreateTweenerDynamicCallFunc(const FTweenDynamicDelegate& InDelegate);
 
